25d661f53a7321b21a13e8c9ddb044ef7b0fee42
jck <jck>
2024-09-27 21:40:43 -0400

    Remove discriminator

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,8 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.discord_username = "#{auth.info.name}##{auth.extra.raw_info.discriminator}"
+      user.discord_username = "#{auth.info.name}"
     end
   end
 end
