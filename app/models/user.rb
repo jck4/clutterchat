b68b2a04de8831b865c17e10163908c6f6fd7d87
jck <jck>
2024-09-27 21:56:27 -0400

    yolo swag

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:discord]
+  devise :omniauthable, omniauth_providers: [:discord]
 
+  # Create or find the user via Discord OAuth
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.password = Devise.friendly_token[0, 20]
       user.discord_username = "#{auth.info.name}"
+      user.password = Devise.friendly_token[0, 20]
     end
   end
 end
